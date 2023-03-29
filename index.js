d74a04119e5fa7b36c096f6df109c28e26e781b0
Genyus <genyus@gmail.com>
2023-03-29 03:56:45 +0100

    chore: initial pseudocode

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const chart = (data) => {};
+const chart = (data) => {
+  // TODO: calculate coordinates
+  //      TODO: calculate max x and max y values
+  //      TODO: calculate x and y axis legend lengths
+  //      TODO: calculate x and y axis legend start points
+  //      TODO: calculate overall x and y dimensions
+  // TODO: render chart
+  //      TODO: render rows
+  //          TODO: render y legend
+  //          TODO: render columns
+  //      TODO: render x legend
+};
 const data = [
   [1, 2],
   [2, 3],
