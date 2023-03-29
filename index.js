3fbde06a801d750e773ba8817e28611542256340
Genyus <genyus@gmail.com>
2023-03-29 04:10:52 +0100

    feat: calculates max x and max y

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const chart = (data) => {
-  // TODO: calculate coordinates
-  //      TODO: calculate max x and max y values
-  //      TODO: calculate x and y axis legend lengths
-  //      TODO: calculate x and y axis legend start points
-  //      TODO: calculate overall x and y dimensions
+  // TODO: calculate key values
+  const calculateKeyValues = () => {
+    const coordinates = { x: {}, y: {} };
+
+    coordinates.x.max = data.reduce(
+      (max, current) => (current[0] > max ? current[0] : max),
+      0
+    );
+    coordinates.y.max = data.reduce(
+      (max, current) => (current[1] > max ? current[1] : max),
+      0
+    );
+
+    // TODO: calculate x and y axis legend lengths
+    // TODO: calculate x and y axis legend start points
+    // TODO: calculate overall x and y dimensions
+
+    return coordinates;
+  };
+  calculateKeyValues();
   // TODO: render chart
   //      TODO: render rows
   //          TODO: render y legend
